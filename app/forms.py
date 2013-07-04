59565a1e62cc29be0993cd173298bb2804681742
mikaylathompson <mikayla.thompson@yale.edu>
2013-07-04 19:18:37 -0400

    Mostly styled, folder pages added, spotty link editing.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form, TextAreaField, TextField, BooleanField, SelectField
-from flask.ext.wtf import Required, Length
+from flask.ext.wtf import Required, Length, ValidationError, URL, InputRequired
 from app.models import User, Link, Folder
 
 class LoginForm(Form):
@@ -26,16 +26,55 @@ class EditForm(Form):
 		return True
 
 class NewLinkForm(Form):
-	title = TextField('title', validators = [Required()])
-	url = TextField('url', validators = [Required()])
+	title = TextField('title', validators = [InputRequired(message='Enter a title.'), ])
+	url = TextField('url', validators = [InputRequired(message='Enter a URL.'), URL(require_tld=False, message="Not a valid URL.")])
 	annotation = TextAreaField('annotation', validators = [Length(min = 0, max = 499)])
-	folder = SelectField('folder') 
+	folder = SelectField('folder', validators = [InputRequired(message='Choose a folder.')]) 
 
 	def validate_on_submit(self):
-		if self.title.data and self.url.data:
-			return True
-		else:
-			return False	
+		if self.title.errors:
+			print 'Error in title.'
+			return False
+		if self.url.errors:
+                        print 'Error in url.'
+                        return False
+		if self.annotation.errors:
+                        print 'Error in annotation.'
+                        return False
+		if self.folder.errors:
+                        print 'Error in folder.'
+                        return False
+		return True
+
+class EditLinkForm(Form):
+	title = TextField('title', validators = [InputRequired(message='Enter a title.'), ])
+	url = TextField('url', validators = [InputRequired(message='Enter a URL.'), URL(require_tld=False, message="Not a valid URL.")])
+	annotation = TextAreaField('annotation', validators = [Length(min = 0, max = 499)])
+	folder = SelectField('folder', validators = [InputRequired(message='Choose a folder.')]) 
+
+	def validate_on_submit(self):
+		if self.title.errors:
+			print 'Error in title.'
+			return False
+		if self.url.errors:
+                        print 'Error in url.'
+                        return False
+		if self.annotation.errors:
+                        print 'Error in annotation.'
+                        return False
+		if self.folder.errors:
+                        print 'Error in folder.'
+                        return False
+		return True
+
+#	def validate_on_submit(self):
+#		if self.title.data and self.url.data and self.folder.data:
+#			return True
+#		if not self.url.data:
+#			self.url.errors.append('Enter a url.')
+#		if not self.title.data:
+#			raise ValidationError('Enter a title.')
+#		return False	
 
 	
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm, NewLinkForm, NewFolder
+from forms import LoginForm, EditForm, NewLinkForm, EditLinkForm, NewFolder
 from models import User, ROLE_USER, ROLE_ADMIN, Link, Folder
 from datetime import datetime
 
@@ -10,23 +10,19 @@ from datetime import datetime
 @login_required
 def index():
 	user = g.user
-	folders = Folder.query.filter_by(user_id = g.user.id).all()
 	#make sure every link is in a folder.
 	for link in user.links:
 		if link.folder_id is None:
 			link.folder_id = 1
 			db.session.add(link)
 	db.session.commit()
+	folders = Folder.query.filter_by(user_id = g.user.id).all()
 	sortedLinks = {}
 	for f in folders:
 		sortedLinks[f] = f.links
-
 	links = Link.query.filter_by(user_id = g.user.id).order_by(Link.timestamp)	
 	return render_template("index.html",
-		title = 'Home',
 		user = user,
-		links = links,
-		folders = folders,
 		sortedLinks = sortedLinks)
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -48,15 +44,35 @@ def logout():
 	return redirect(url_for('index'))
 
 @app.route('/user/<name>')
-@login_required
 def user(name):
 	user = User.query.filter_by(name = name).first()
 	if user == None:
 		flash('User' + name + ' not found.')
 		return redirect(url_for('index'))
+	folders = Folder.query.filter_by(user_id = user.id).all()
+	sortedLinks = {}
+	for f in folders:
+		sortedLinks[f] = f.links
 	links = Link.query.filter_by(user_id = user.id).order_by(Link.timestamp)	
 	folders = Folder.query.filter_by(user_id = user.id).all()
-	return render_template('user.html', user = user, links = links, folders = folders)
+	return render_template('user.html', 
+		user = user, 
+		sortedLinks = sortedLinks)
+
+@app.route('/user/<name>/folder/<label>')
+def folder(name, label):
+	user = User.query.filter_by(name = name).first()
+	if user == None:
+		flash('User' + name + ' not found.')
+		return redirect(url_for('index'))
+	folder = Folder.query.filter_by(label = label, user_id=user.id).first()
+	if folder == None:
+		flash(name + ' doesn\'t have a folder of that name.')
+		return redirect(url_for('user', name=name))
+	return render_template('folder.html', 
+		user = user, 
+		folder = folder)
+
 
 @app.route('/edit', methods = ['GET', 'POST'])
 @login_required
@@ -74,6 +90,40 @@ def edit():
 		form.about_me.data = g.user.about_me
 	return render_template('edit.html', user = g.user, form = form)
 
+@app.route('/link/<id>', methods = ['GET', 'POST'])
+@login_required
+def editlink(id):
+	link = Link.query.get(id)
+	if link == None:
+		flash('That link doesn\'t exist.')
+		return redirect(url_for('index', user = g.user))
+	if link.user_id == g.user.id:
+		form = EditLinkForm()
+		if form.validate_on_submit():
+			if form.url.data != link.url and form.url.data != None:
+				url_raw = form.url.data
+				if url_raw[0:7] == 'http://' or url_raw[0:8] == 'https://':
+					url_clean = url_raw
+				else:
+					url_clean = 'http://' + url_raw
+				link.folder = form.url_clean
+			link.title = form.title.data
+			link.annotation = form.annotation.data
+			print "CAN ANYONE HEAR ME?!?"
+			print form.folder.data
+			if form.folder.data != None:
+				link.folder = int(form.folder.data)
+			db.session.add(link)
+			db.session.commit()
+			flash('Your changes have been saved.')
+			return redirect(url_for('user', name = g.user.name))
+		else:
+			form.title.data = link.title
+			form.url.data = link.url
+			form.annotation.data = link.annotation
+			# form.folder.data = link.folder
+	return render_template('link.html', link = link, user = g.user, form = form)
+
 @app.route('/newlink', methods = ['GET', 'POST'])
 @login_required
 def newlink():
@@ -88,10 +138,10 @@ def newlink():
 				url_clean = 'http://' + url_raw
 			title = form.title.data
 			annotation = form.annotation.data
-			if form.folder.data:
-				folder = int(form.folder.data)
-			else:
+			if form.folder.data == None:
 				folder = 1
+			else:
+				folder = int(form.folder.data)
 			timestamp = datetime.utcnow()
 			user_id = g.user.id
 			link = Link(title = title,
@@ -111,13 +161,14 @@ def newlink():
 @login_required
 def newfolder():
 	form = NewFolder()
-	if form.validate_on_submit():
-		folder = Folder(label = form.label.data, user_id = g.user.id)
-		db.session.add(folder)
-		db.session.commit()
-		flash('Folder added.')
-		return redirect(url_for('index'))
-	flash('The form wasn\'t created.  Try again.')
+	if form.label.data:
+		if form.validate_on_submit():
+			folder = Folder(label = form.label.data, user_id = g.user.id)
+			db.session.add(folder)
+			db.session.commit()
+			flash('Folder added.')
+			return redirect(url_for('index'))
+		flash('The form wasn\'t created.  Try again.')
 	return render_template('newfolder.html', user = g.user, form = form)
 
 
